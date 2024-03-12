bc10543058c2c489a2021aca4b3ccb675f47b752
ssofiica <s.valova2016@yandex.ru>
2024-03-12 12:14:03 +0300

    fix

--- internal/delivery/restaurants/handlers_test.go
+++ internal/delivery/restaurants/handlers_test.go
@@ -1,6 +1,7 @@
 package delivery
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -13,31 +14,46 @@ type TestCase struct {
 }
 
 func TestRests(t *testing.T) {
-	cases := []TestCase{
-		TestCase{
-			Response:   `[{"id":1,"name":"Пицца 22 см","description":"Пиццерия с настоящей неаполитанской пиццей","img_url":"assets/mocks/restaurants/1.jpg"},{"id":2,"name":"Bro\u0026N","description":"Ресторан классической итальянской кухни","img_url":"assets/mocks/restaurants/2.jpg"},{"id":3,"name":"#FARШ","description":"Сеть бургерных с сочным мясом от \"Мираторга\"","img_url":"assets/mocks/restaurants/3.jpg"},{"id":4,"name":"Loona","description":"Итальянскую классику в современном прочтении","img_url":"assets/mocks/restaurants/4.jpg"},{"id":5,"name":"Pino","description":"Обширное интернациональное меню","img_url":"assets/mocks/restaurants/5.jpg"},{"id":6,"name":"Sage","description":"Авторская евпропейская кухня с акцентом на мясо и рыбу","img_url":"assets/mocks/restaurants/6.jpg"},{"id":7,"name":"TECHNIKUM","description":"Современное гастробистро с нескучной едой","img_url":"assets/mocks/restaurants/7.jpg"}]`,
-			StatusCode: http.StatusOK,
-		},
+	casee := TestCase{
+		Response:   `[{"id":1,"name":"Пицца 22 см","description":"Пиццерия с настоящей неаполитанской пиццей","img_url":"assets/mocks/restaurants/1.jpg"},{"id":2,"name":"Bro\u0026N","description":"Ресторан классической итальянской кухни","img_url":"assets/mocks/restaurants/2.jpg"},{"id":3,"name":"#FARШ","description":"Сеть бургерных с сочным мясом от \"Мираторга\"","img_url":"assets/mocks/restaurants/3.jpg"},{"id":4,"name":"Loona","description":"Итальянскую классику в современном прочтении","img_url":"assets/mocks/restaurants/4.jpg"},{"id":5,"name":"Pino","description":"Обширное интернациональное меню","img_url":"assets/mocks/restaurants/5.jpg"},{"id":6,"name":"Sage","description":"Авторская евпропейская кухня с акцентом на мясо и рыбу","img_url":"assets/mocks/restaurants/6.jpg"},{"id":7,"name":"TECHNIKUM","description":"Современное гастробистро с нескучной едой","img_url":"assets/mocks/restaurants/7.jpg"}]`,
+		StatusCode: http.StatusOK,
 	}
 	rests := NewRestaurantStore()
-	for _, item := range cases {
-		url := "https://resto-go.ru/restaurants"
-		req := httptest.NewRequest("GET", url, nil)
-		w := httptest.NewRecorder()
+	url := "https://resto-go.ru/restaurants"
+	req := httptest.NewRequest("GET", url, nil)
+	w := httptest.NewRecorder()
+	rests.RestaurantList(w, req)
+	if w.Code != casee.StatusCode {
+		t.Errorf("wrong StatusCode: got %d, expected %d",
+			w.Code, casee.StatusCode)
+	}
+	resp := w.Result()
+	body, _ := io.ReadAll(resp.Body)
+	bodyStr := string(body)
+	if bodyStr != casee.Response {
+		t.Errorf("wrong Response: got %+v, expected %+v", bodyStr, casee.Response)
+	}
+}
 
-		rests.RestaurantList(w, req)
-
-		if w.Code != item.StatusCode {
-			t.Errorf("wrong StatusCode: got %d, expected %d",
-				w.Code, item.StatusCode)
-		}
-
-		resp := w.Result()
-		body, _ := io.ReadAll(resp.Body)
-		bodyStr := string(body)
-		if bodyStr != item.Response {
-			t.Errorf("wrong Response: got %+v, expected %+v",
-				bodyStr, item.Response)
-		}
+func TestError(t *testing.T) {
+	casee := TestCase{
+		Response:   ``,
+		StatusCode: http.StatusOK,
+	}
+	fmt.Printf("", casee)
+	rests := NewRestaurantStore()
+	url := "https://resto-go.ru/restaurants"
+	req := httptest.NewRequest("GET", url, nil)
+	w := httptest.NewRecorder()
+	rests.RestaurantList(w, req)
+	if w.Code != casee.StatusCode {
+		t.Errorf("wrong StatusCode: got %d, expected %d",
+			w.Code, casee.StatusCode)
+	}
+	resp := w.Result()
+	body, _ := io.ReadAll(resp.Body)
+	bodyStr := string(body)
+	if bodyStr != casee.Response {
+		t.Errorf("wrong Response: got %+v, expected %+v", bodyStr, casee.Response)
 	}
 }
