253e2ec7cad8ed399576ea17ce8101c3e2c8cca5
Lobanov Ivan <physic2003@mail.ru>
2024-03-30 16:00:41 +0300

    update food and rest repos

--- internal/repository/food/repo.go
+++ internal/repository/food/repo.go
@@ -5,34 +5,35 @@ import (
 	"database/sql"
 
 	"2024_1_kayros/internal/entity"
+	"2024_1_kayros/internal/utils/alias"
 )
 
-type RepoInterface interface {
-	GetByRest(ctx context.Context, restId int) ([]*entity.Food, error)
-	GetById(ctx context.Context, id int) (*entity.Food, error)
-	AddToOrder(ctx context.Context, foodId int, orderId int) error
-	UpdateCountInOrder(ctx context.Context, foodId int, orderId int, count int) error
-	DeleteFromOrder(ctx context.Context, foodId int, orderId int) error
+type Repo interface {
+	GetByRestId(context.Context, alias.RestId) ([]*entity.Food, error)
+	GetById(context.Context, alias.FoodId) (*entity.Food, error)
+	AddToOrder(context.Context, alias.FoodId, alias.OrderId) (bool, error)
+	UpdateCountInOrder(context.Context, alias.FoodId, alias.OrderId, uint32) (bool, error)
+	DeleteFromOrder(context.Context, alias.FoodId, alias.OrderId) (bool, error)
 }
 
-type Repo struct {
+type RepoLayer struct {
 	DB *sql.DB
 }
 
-func NewFoodRepository(db *sql.DB) *Repo {
-	return &Repo{DB: db}
+func NewRepoLayer(db *sql.DB) Repo {
+	return &RepoLayer{DB: db}
 }
 
-func (repo *Repo) GetByRest(ctx context.Context, restId int) ([]*entity.Food, error) {
+func (repo *RepoLayer) GetByRestId(ctx context.Context, id alias.RestId) ([]*entity.Food, error) {
 	var food []*entity.Food
-	rows, err := repo.DB.QueryContext(ctx, "SELECT id, name, img_url, price, weight FROM food WHERE restaurant = $1", restId)
+	rows, err := repo.DB.QueryContext(ctx, "SELECT id, name, img_url, price, weight FROM food WHERE restaurant = $1", uint64(id))
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 	for rows.Next() {
 		item := &entity.Food{}
-		err = rows.Scan(&item.Id, &item.Name, &item.ImgUrl, &item.Price, &item.Weight)
+		err = rows.Scan(item.Id, item.Name, item.ImgUrl, item.Price, item.Weight)
 		if err != nil {
 			return nil, err
 		}
@@ -41,36 +42,36 @@ func (repo *Repo) GetByRest(ctx context.Context, restId int) ([]*entity.Food, er
 	return food, nil
 }
 
-func (repo *Repo) GetById(ctx context.Context, id int) (*entity.Food, error) {
+func (repo *RepoLayer) GetById(ctx context.Context, id alias.FoodId) (*entity.Food, error) {
 	item := &entity.Food{}
 	row := repo.DB.QueryRowContext(ctx, "SELECT id, name, img_url, price, weight FROM food WHERE id=$1", id)
-	err := row.Scan(&item.Id, &item.Name, &item.ImgUrl, &item.Price, &item.Weight)
+	err := row.Scan(item.Id, item.Name, item.ImgUrl, item.Price, item.Weight)
 	if err != nil {
 		return nil, err
 	}
 	return item, nil
 }
 
-func (repo *Repo) AddToOrder(ctx context.Context, foodId int, orderId int) error {
+func (repo *RepoLayer) AddToOrder(ctx context.Context, foodId alias.FoodId, orderId alias.OrderId) (bool, error) {
 	_, err := repo.DB.ExecContext(ctx, "INSERT INTO food-in-order (food_id, order_id, count) VALUES ($1, $2, 1)", foodId, orderId)
 	if err != nil {
-		return err
+		return false, err
 	}
-	return nil
+	return true, nil
 }
 
-func (repo *Repo) UpdateCountInOrder(ctx context.Context, foodId int, orderId int, count int) error {
+func (repo *RepoLayer) UpdateCountInOrder(ctx context.Context, foodId alias.FoodId, orderId alias.OrderId, count uint32) (bool, error) {
 	_, err := repo.DB.ExecContext(ctx, "UPDATE food-in-order SET count=$1 WHERE order_id=$2 AND food_id=$3", count, orderId, foodId)
 	if err != nil {
-		return err
+		return false, err
 	}
-	return nil
+	return true, nil
 }
 
-func (repo *Repo) DeleteFromOrder(ctx context.Context, foodId int, orderId int) error {
+func (repo *RepoLayer) DeleteFromOrder(ctx context.Context, foodId alias.FoodId, orderId alias.OrderId) (bool, error) {
 	_, err := repo.DB.ExecContext(ctx, "DELETE FROM food-in-order WHERE order_id=$1 AND food_id=$2", orderId, foodId)
 	if err != nil {
-		return err
+		return false, err
 	}
-	return nil
+	return true, nil
 }
--- internal/repository/restaurants/repo.go
+++ internal/repository/restaurants/repo.go
@@ -5,22 +5,23 @@ import (
 	"database/sql"
 
 	"2024_1_kayros/internal/entity"
+	"2024_1_kayros/internal/utils/alias"
 )
 
-type RestaurantRepoInterface interface {
-	GetAll(ctx context.Context) ([]*entity.Restaurant, error)
-	GetById(ctx context.Context, id int) (*entity.Restaurant, error)
+type Repo interface {
+	GetAll(context.Context) ([]*entity.Restaurant, error)
+	GetById(context.Context, alias.RestId) (*entity.Restaurant, error)
 }
 
-type RestaurantRepo struct {
+type RepoLayer struct {
 	DB *sql.DB
 }
 
-func NewRestaurantRepo(db *sql.DB) *RestaurantRepo {
-	return &RestaurantRepo{DB: db}
+func NewRepoLayer(db *sql.DB) Repo {
+	return &RepoLayer{DB: db}
 }
 
-func (repo *RestaurantRepo) GetAll(ctx context.Context) ([]*entity.Restaurant, error) {
+func (repo *RepoLayer) GetAll(ctx context.Context) ([]*entity.Restaurant, error) {
 	var rests []*entity.Restaurant
 	rows, err := repo.DB.QueryContext(ctx, "SELECT id, name, short_description, img_url FROM Restaurant")
 	if err != nil {
@@ -30,7 +31,7 @@ func (repo *RestaurantRepo) GetAll(ctx context.Context) ([]*entity.Restaurant, e
 	defer rows.Close()
 	for rows.Next() {
 		rest := &entity.Restaurant{}
-		err = rows.Scan(&rest.Id, &rest.Name, &rest.ShortDescription, &rest.ImgUrl)
+		err = rows.Scan(rest.Id, rest.Name, rest.ShortDescription, rest.ImgUrl)
 		if err != nil {
 			return nil, err
 		}
@@ -39,10 +40,10 @@ func (repo *RestaurantRepo) GetAll(ctx context.Context) ([]*entity.Restaurant, e
 	return rests, nil
 }
 
-func (repo *RestaurantRepo) GetById(ctx context.Context, id int) (*entity.Restaurant, error) {
+func (repo *RepoLayer) GetById(ctx context.Context, id alias.RestId) (*entity.Restaurant, error) {
 	rest := &entity.Restaurant{}
-	row := repo.DB.QueryRowContext(ctx, "SELECT id, name, long_description, img_url FROM Restaurant WHERE id=$1", id)
-	err := row.Scan(&rest.Id, &rest.Name, &rest.LongDescription, &rest.ImgUrl)
+	row := repo.DB.QueryRowContext(ctx, "SELECT id, name, long_description, img_url FROM Restaurant WHERE id=$1", uint64(id))
+	err := row.Scan(rest.Id, rest.Name, rest.LongDescription, rest.ImgUrl)
 	if err != nil {
 		return nil, err
 	}
