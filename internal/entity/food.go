d8982d1417b3c58683bd3ea952185e16c5b927fa
Lobanov Ivan <physic2003@mail.ru>
2024-04-02 22:26:53 +0300

    improve food db queries

--- internal/entity/food.go
+++ internal/entity/food.go
@@ -1,11 +1,12 @@
 package entity
 
 type Food struct {
-	Id          uint64
-	Name        string
-	Description string
-	Restaurant  uint64
-	ImgUrl      string
-	Weight      uint64
-	Price       uint64
+	Id           uint64
+	Name         string
+	Description  string
+	RestaurantId uint64
+	CategoryId   uint64
+	Weight       uint64
+	Price        uint64
+	ImgUrl       string
 }
--- internal/repository/food/repo.go
+++ internal/repository/food/repo.go
@@ -3,6 +3,7 @@ package food
 import (
 	"context"
 	"database/sql"
+	"errors"
 
 	"2024_1_kayros/internal/entity"
 	"2024_1_kayros/internal/utils/alias"
@@ -23,27 +24,38 @@ func NewRepoLayer(dbProps *sql.DB) Repo {
 
 func (repo *RepoLayer) GetByRestId(ctx context.Context, restId alias.RestId) ([]*entity.Food, error) {
 	var food []*entity.Food
-	rows, err := repo.db.QueryContext(ctx, `SELECT id, name, description, restaurant, img_url, weight, price FROM "Food" WHERE restaurant = $1`, uint64(restId))
+	rows, err := repo.db.QueryContext(ctx,
+		`SELECT id, name, description, restaurant_id, category_id, weight, price, img_url 
+				FROM "Food" WHERE restaurant_id = $1`, uint64(restId))
 	if err != nil {
 		return nil, err
 	}
+
 	for rows.Next() {
-		item := &entity.Food{}
-		err = rows.Scan(item.Id, item.Name, item.ImgUrl, item.Price, item.Weight)
+		item := entity.Food{}
+		err = rows.Scan(&item.Id, &item.Name, &item.Description, &item.RestaurantId,
+			&item.CategoryId, &item.ImgUrl, &item.Price, &item.Weight)
 		if err != nil {
 			return nil, err
 		}
-		food = append(food, item)
+		food = append(food, &item)
 	}
 	return food, nil
 }
 
 func (repo *RepoLayer) GetById(ctx context.Context, foodId alias.FoodId) (*entity.Food, error) {
-	item := &entity.Food{}
-	row := repo.db.QueryRowContext(ctx, `SELECT id, name, description, restaurant, img_url, weight, price FROM "Food" WHERE id=$1`, uint64(foodId))
-	err := row.Scan(item.Id, item.Name, item.ImgUrl, item.Price, item.Weight)
+	row := repo.db.QueryRowContext(ctx,
+		`SELECT id, name, description, restaurant_id, category_id, weight, price, img_url 
+				FROM "Food" WHERE id=$1`, uint64(foodId))
+
+	item := entity.Food{}
+	err := row.Scan(&item.Id, &item.Name, &item.Description, &item.RestaurantId,
+		&item.CategoryId, &item.ImgUrl, &item.Price, &item.Weight)
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, nil
+	}
 	if err != nil {
 		return nil, err
 	}
-	return item, nil
+	return &item, nil
 }
