1e506be58cdd718a1946aba8f89fd312f55962e4
Lobanov Ivan <physic2003@mail.ru>
2024-04-11 23:06:00 +0300

    add internal schema - ddl

---
+++ db/normalized/schema.sql
@@ -0,0 +1,79 @@
+-- Определение схемы
+SET schema 'public';
+
+-- Конфигурация
+SET BYTEA_OUTPUT TO 'hex';
+
+CREATE TABLE IF NOT EXISTS "user"
+(
+    id          INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name        TEXT CONSTRAINT name_domain CHECK (LENGTH(name) BETWEEN 2 AND 20) NOT NULL,
+    email       TEXT CONSTRAINT email_domain CHECK (LENGTH(email) BETWEEN 6 AND 50) UNIQUE NOT NULL,
+    phone       TEXT CONSTRAINT phone_domain CHECK (LENGTH(phone) = 18), -- формат |+7 (989) 232 12 12|
+    password    BYTEA CONSTRAINT password_domain CHECK (LENGTH(password) = 64) NOT NULL,
+    address     TEXT CONSTRAINT address_domain CHECK (LENGTH(address) BETWEEN 14 AND 100),     -- |ул. Мира, д. 4| (самое короткое название улицы в Москве 4 символа)
+    img_url     TEXT CONSTRAINT img_url_domain CHECK(LENGTH(img_url) <= 60) NOT NULL DEFAULT '/minio-api/users/default.jpg',  -- для генерации названия картинки используется uuid.V4 (36 символов в длину)
+    card_number BYTEA CONSTRAINT card_number_domain CHECK (LENGTH(password) = 64),
+    created_at  TIMESTAMPTZ CONSTRAINT time_create_domain NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMPTZ CONSTRAINT time_last_updated_domain NOT NULL DEFAULT CURRENT_TIMESTAMP
+
+);
+
+CREATE TABLE IF NOT EXISTS category
+(
+    id   INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name TEXT CONSTRAINT category_domain CHECK (LENGTH(name) BETWEEN 2 AND 30) UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS restaurant
+(
+    id                INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name              TEXT CONSTRAINT name_domain CHECK (LENGTH(name) BETWEEN 2 AND 30) UNIQUE NOT NULL,
+    short_description TEXT CONSTRAINT short_description_domain CHECK (LENGTH(short_description) BETWEEN 10 AND 100) NOT NULL,
+    long_description  TEXT CONSTRAINT long_description_domain CHECK (LENGTH(long_description) BETWEEN 20 AND 250) NOT NULL,
+    img_url           TEXT CONSTRAINT img_url_domain CHECK(LENGTH(img_url) <= 60) NOT NULL DEFAULT '/minio-api/restaurants/default.jpg'
+);
+
+CREATE TABLE IF NOT EXISTS restaurant_address (
+    restaurant_id     INT  GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    address           TEXT CONSTRAINT rest_address CHECK (LENGTH(address) BETWEEN 14 AND 100) NOT NULL,
+    CONSTRAINT rest_unique UNIQUE (restaurant_id, address)
+);
+
+CREATE TABLE IF NOT EXISTS "order"
+(
+    id             INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id        INTEGER CONSTRAINT foreign_key CHECK (user_id > 0) NOT NULL REFERENCES "user" (id) ON DELETE CASCADE,
+    sum            INTEGER CONSTRAINT positive_sum CHECK (sum >= 0) NOT NULL DEFAULT 0,
+    status         TEXT CONSTRAINT order_status_domain NOT NULL DEFAULT 'draft',
+    address        TEXT CONSTRAINT order_address_length CHECK (LENGTH(address) BETWEEN 14 AND 100),
+    extra_address  TEXT CONSTRAINT order_extra_address_length CHECK (LENGTH(address) BETWEEN 2 AND 30),
+    created_at     TIMESTAMPTZ CONSTRAINT user_time_create NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at     TIMESTAMPTZ CONSTRAINT user_time_last_updated NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    received_at    TIMESTAMPTZ
+);
+
+-- БЖУ хранятся в МГ
+CREATE TABLE IF NOT EXISTS food(
+    id            INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    restaurant_id INTEGER CONSTRAINT foreign_key_rest CHECK (restaurant_id > 0) NOT NULL REFERENCES restaurant (id) ON DELETE CASCADE,
+    category_id   INTEGER CONSTRAINT foreign_key_category CHECK (category_id > 0) NOT NULL REFERENCES category (id) ON DELETE CASCADE,
+    name          TEXT CONSTRAINT name_domain CHECK (LENGTH(name) BETWEEN 2 AND 60) NOT NULL,
+    weight        INTEGER CONSTRAINT positive_weight CHECK (weight > 0) NOT NULL,
+    price         INTEGER CONSTRAINT positive_price CHECK (price > 0) NOT NULL,
+    proteins      NUMERIC CONSTRAINT non_negative_proteins CHECK (proteins >= 0) NOT NULL,
+    fats          NUMERIC CONSTRAINT non_negative_fats CHECK (fats >= 0) NOT NULL,
+    carbohydrates NUMERIC CONSTRAINT non_negative_carb CHECK (carbohydrates >= 0) NOT NULL,
+    img_url       TEXT CONSTRAINT img_url_domain CHECK(LENGTH(img_url) <= 60) DEFAULT '/minio-api/foods/default.jpg' NOT NULL,
+    CONSTRAINT unique_food_in_rests UNIQUE (name, restaurant_id)
+);
+
+CREATE TABLE IF NOT EXISTS food_order
+(
+    food_id     INTEGER CONSTRAINT foreign_key_food CHECK (food_id > 0) NOT NULL REFERENCES food (id) ON DELETE CASCADE,
+    order_id    INTEGER CONSTRAINT foreign_key_order CHECK (order_id > 0) NOT NULL REFERENCES "order" (id) ON DELETE CASCADE,
+    count       INTEGER CONSTRAINT food_count_in_order CHECK (count > 0) NOT NULL,
+    created_at  TIMESTAMPTZ CONSTRAINT time_create NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMPTZ CONSTRAINT last_updated NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (food_id, order_id)
+);
--- integration/postgres/docker-entrypoint-initdb.d/data-init.sql
+++ integration/postgres/docker-entrypoint-initdb.d/data-init.sql
@@ -1,17 +1,23 @@
 -- Заполнение БД данными
-INSERT INTO "user" (name, email, password, address, created_at, updated_at)
-VALUES ('Иван', 'ivan@example.com', E'\\x6AAA04147C1662A5D5608B40030CD4163F0ABAB80BF1B4F37863278237FB3F429DFA3527143F96DBB01971553A70EAF79D592B5BAFB4D229DF16EA67833D69F7',
-        'ул. Ленина, д. 1', current_timestamp, current_timestamp),
-       ('Анна', 'anna@example.com', E'\\x6AAA04147C1662A5D5608B40030CD4163F0ABAB80BF1B4F37863278237FB3F429DFA3527143F96DBB01971553A70EAF79D592B5BAFB4D229DF16EA67833D69F7',
-        'пр. Победы, д. 10', current_timestamp, current_timestamp),
-       ('Петр', 'petr@example.com', E'\\x6AAA04147C1662A5D5608B40030CD4163F0ABAB80BF1B4F37863278237FB3F429DFA3527143F96DBB01971553A70EAF79D592B5BAFB4D229DF16EA67833D69F7',
-        'ул. Мира, д. 5', current_timestamp, current_timestamp),
-       ('Мария', 'maria@example.com', E'\\x6AAA04147C1662A5D5608B40030CD4163F0ABAB80BF1B4F37863278237FB3F429DFA3527143F96DBB01971553A70EAF79D592B5BAFB4D229DF16EA67833D69F7',
-        'пр. Ленинградский, д. 15', current_timestamp, current_timestamp),
-       ('Алексей', 'alex@example.com', E'\\x6AAA04147C1662A5D5608B40030CD4163F0ABAB80BF1B4F37863278237FB3F429DFA3527143F96DBB01971553A70EAF79D592B5BAFB4D229DF16EA67833D69F7',
-        'ул. Советская, д. 25', current_timestamp, current_timestamp),
-       ('Елена', 'elena@example.com', E'\\x6AAA04147C1662A5D5608B40030CD4163F0ABAB80BF1B4F37863278237FB3F429DFA3527143F96DBB01971553A70EAF79D592B5BAFB4D229DF16EA67833D69F7',
-        'пр. Гагарина, д. 30', current_timestamp, current_timestamp);
+INSERT INTO "user" (name, email, password, address)
+VALUES ('Иван', 'ivan@example.com',
+        '\x6AAA04147C1662A5D5608B40030CD4163F0ABAB80BF1B4F37863278237FB3F429DFA3527143F96DBB01971553A70EAF79D592B5BAFB4D229DF16EA67833D69F7'::BYTEA,
+        'ул. Ленина, д. 1'),
+       ('Анна', 'anna@example.com',
+        '\x6AAA04147C1662A5D5608B40030CD4163F0ABAB80BF1B4F37863278237FB3F429DFA3527143F96DBB01971553A70EAF79D592B5BAFB4D229DF16EA67833D69F7'::BYTEA,
+        'пр. Победы, д. 10'),
+       ('Петр', 'petr@example.com',
+        '\x6AAA04147C1662A5D5608B40030CD4163F0ABAB80BF1B4F37863278237FB3F429DFA3527143F96DBB01971553A70EAF79D592B5BAFB4D229DF16EA67833D69F7'::BYTEA,
+        'ул. Мира, д. 5'),
+       ('Мария', 'maria@example.com',
+        '\x6AAA04147C1662A5D5608B40030CD4163F0ABAB80BF1B4F37863278237FB3F429DFA3527143F96DBB01971553A70EAF79D592B5BAFB4D229DF16EA67833D69F7'::BYTEA,
+        'пр. Ленинградский, д. 15'),
+       ('Алексей', 'alex@example.com',
+        '\x6AAA04147C1662A5D5608B40030CD4163F0ABAB80BF1B4F37863278237FB3F429DFA3527143F96DBB01971553A70EAF79D592B5BAFB4D229DF16EA67833D69F7'::BYTEA,
+        'ул. Советская, д. 25'),
+       ('Елена', 'elena@example.com',
+        '\x6AAA04147C1662A5D5608B40030CD4163F0ABAB80BF1B4F37863278237FB3F429DFA3527143F96DBB01971553A70EAF79D592B5BAFB4D229DF16EA67833D69F7'::BYTEA,
+        'пр. Гагарина, д. 30');
 
 -- Вставка данных в таблицу "Category"
 INSERT INTO category (name)
@@ -35,13 +41,13 @@ VALUES ('Ресторан "Вкусняшка"', 'Лучший ресторан 
         'У нас вы найдете вкусные домашние блюда и дружелюбный персонал', 'пл. Пушкина, д. 10');
 
 -- Вставка данных в таблицу "Order"
-INSERT INTO "order" (user_id, created_at, updated_at, status, address, extra_address, sum)
-VALUES (1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'processing', 'ул. Ленина, д. 1', NULL, 1500),
-       (2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'draft', 'пр. Победы, д. 10', NULL, 2000),
-       (3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'completed', 'ул. Мира, д. 5', NULL, 1000),
-       (4, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'processing', 'пр. Ленинградский, д. 15', NULL, 1200),
-       (5, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'draft', 'ул. Советская, д. 25', NULL, 1800),
-       (6, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'completed', 'пр. Гагарина, д. 30', NULL, 900);
+INSERT INTO "order" (user_id, status, address, sum)
+VALUES (1, 'processing', 'ул. Ленина, д. 1', 1500),
+       (2, 'draft', 'пр. Победы, д. 10', 2000),
+       (3, 'completed', 'ул. Мира, д. 5', 1000),
+       (4, 'processing', 'пр. Ленинградский, д. 15', 1200),
+       (5, 'draft', 'ул. Советская, д. 25', 1800),
+       (6, 'completed', 'пр. Гагарина, д. 30', 900);
 
 -- Вставка данных в таблицу "Food"
 INSERT INTO food (name, description, restaurant_id, category_id, weight, price, proteins, fats, carbohydrates)
@@ -53,9 +59,9 @@ VALUES ('Омлет', 'С яйцами и овощами', 1, 1, 200, 300, 15, 1
        ('Картофель фри', 'Жареный картофель в виде палочек', 1, 2, 200, 300, 5, 10, 25);
 
 -- Вставка данных в таблицу "FoodOrder"
-INSERT INTO food_order (food_id, order_id, count, created_at, updated_at)
-VALUES (1, 1, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-       (2, 2, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-       (3, 3, 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-       (4, 1, 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO food_order (food_id, order_id, count)
+VALUES (1, 1, 2),
+       (2, 2, 1),
+       (3, 3, 3),
+       (4, 1, 3);
 
