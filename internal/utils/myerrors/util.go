3a49fef981f583bc88557a15a75d45d4124aaebc
Lobanov Ivan <physic2003@mail.ru>
2024-04-12 13:05:18 +0300

    change the name of auth error

--- internal/utils/myerrors/util.go
+++ internal/utils/myerrors/util.go
@@ -1,13 +1,18 @@
 package myerrors
 
+// Ошибки сетевых ответов
 const (
 	InternalServerError     = "Ошибка сервера"
-	HashedPasswordError     = "Не удалось захешировать пароль пользователя"
-	UnauthorizedError       = "Не хватает прав для доступа"
+	UnauthorizedError       = "Вы не зарегистрированы"
 	BadAuthCredentialsError = "Неверный логин или пароль"
 	BadCredentialsError     = "Предоставлены некорректные данные"
 	UserAlreadyExistError   = "Пользователь с таким логином уже зарегистрирован"
 	BigSizeFileError        = "Превышен максимальный размер файла"
 	NotFoundError           = "Данные не найдены"
 	EqualPasswordsError     = "Новый пароль совпадает со старым"
+)
+
+// Внутренние ошибки
+const (
+	HashedPasswordError = "Не удалось захешировать пароль пользователя"
 )
