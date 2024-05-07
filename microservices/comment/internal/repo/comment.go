7f82c67e6b935f64213a142268126133ec0038a5
Lobanov Ivan <physic2003@mail.ru>
2024-05-07 21:57:59 +0300

    fix comment

--- microservices/comment/internal/repo/comment.go
+++ microservices/comment/internal/repo/comment.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 
+	"2024_1_kayros/internal/utils/functions"
 	"2024_1_kayros/internal/utils/myerrors"
 	comment "2024_1_kayros/microservices/comment/proto"
 )
@@ -29,9 +30,8 @@ func NewCommentLayer(dbProps *sql.DB) Comment {
 func (repo *CommentLayer) Create(ctx context.Context, com *comment.Comment) (*comment.Comment, error) {
 	var id uint64
 	err := repo.db.QueryRowContext(ctx,
-		`INSERT INTO "comment" (user_id, restaurant_id, text, rating) VALUES ($1, $2, $3, $4) RETURNING id`, com.UserId, com.RestId, com.Text, com.Rating).Scan(&id)
+		`INSERT INTO "comment" (user_id, restaurant_id, text, rating) VALUES ($1, $2, $3, $4) RETURNING id`, com.UserId, com.RestId, functions.MaybeNullString(com.Text), com.Rating).Scan(&id)
 	if err != nil {
-		fmt.Println(err)
 		if errors.Is(err, sql.ErrNoRows) {
 			return nil, myerrors.SqlNoRowsCommentRelation
 		}
@@ -43,7 +43,6 @@ func (repo *CommentLayer) Create(ctx context.Context, com *comment.Comment) (*co
 		`SELECT rating, comment_count FROM restaurant WHERE id=$1`, com.RestId)
 	err = row.Scan(&rest.Rating, &rest.CommentCount)
 	if err != nil {
-		fmt.Println(err)
 		if errors.Is(err, sql.ErrNoRows) {
 			return nil, myerrors.SqlNoRowsCommentRelation
 		}
@@ -99,9 +98,8 @@ func (repo *CommentLayer) Create(ctx context.Context, com *comment.Comment) (*co
 
 func (repo *CommentLayer) GetCommentsByRest(ctx context.Context, restId *comment.RestId) (*comment.CommentList, error) {
 	rows, err := repo.db.QueryContext(ctx,
-		`SELECT c.id, u.name, u.img_url, c.text, c.rating FROM "comment" AS c JOIN "user" AS u ON c.user_id = u.id WHERE restaurant_id=$1`, restId.Id)
+		`SELECT c.id, u.name, u.img_url, c.text, c.rating FROM "comment" AS c JOIN "user" AS u ON c.user_id = u.id WHERE restaurant_id=$1 AND c.text IS NOT NULL AND c.text !=''`, restId.Id)
 	if err != nil {
-		fmt.Println(err)
 		return nil, err
 	}
 	comments := comment.CommentList{}
@@ -109,7 +107,6 @@ func (repo *CommentLayer) GetCommentsByRest(ctx context.Context, restId *comment
 		com := comment.Comment{}
 		err = rows.Scan(&com.Id, &com.UserName, &com.Image, &com.Text, &com.Rating)
 		if err != nil {
-			fmt.Println(err)
 			return nil, err
 		}
 		comments.Comment = append(comments.Comment, &com)
